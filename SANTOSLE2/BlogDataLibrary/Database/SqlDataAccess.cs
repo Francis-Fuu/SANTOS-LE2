c1d5b1395623258ca81534c66db8bf93bbf680e0
Francis-Fuu <90777109+Francis-Fuu@users.noreply.github.com>
2023-04-21 21:06:29 +0800

    Add files via upload

--- SANTOSLE2/BlogDataLibrary/Database/SqlDataAccess.cs
+++ SANTOSLE2/BlogDataLibrary/Database/SqlDataAccess.cs
@@ -1,39 +1,45 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BlogDataLibrary.Database
 {
-    public class SqlDataAccess : SqlDataAccess, ISqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
         private IConfiguration _config;
-
         public SqlDataAccess(IConfiguration config)
         {
             _config = config;
         }
+
         public List<T> LoadData<T, U>(string sqlStatement,
-  U Parameters, string connectionStringName, bool isStoredProcedure)
+            U parameters, string connectStringName, bool isStoredProcedure)
         {
             CommandType commandType = CommandType.Text;
-            string connectionString = _config.GetConnectionString(connectionStringName);
+            string connectionString = _config.GetConnectionString(connectStringName);
 
             if (isStoredProcedure)
             {
                 commandType = CommandType.StoredProcedure;
             }
 
-            using (IDbConnection connection = new SqlConnection(connectionString))
+            using (IDbConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = BlogDB; Integrated Security=True;Connect Timeout=60;"))
             {
-                List<T> rows = connection.Query<T>(sqlStatement, Parameters,
-                  commandType: commandType).ToList();
+                List<T> rows = connection.Query<T>(sqlStatement, parameters,
+                    commandType: commandType).ToList();
+
                 return rows;
             }
         }
 
         public void SaveData<T>(string sqlStatement,
-          T parameters, string connectionStringName, bool isStoredProcedure)
+            T parameters, string connectionStringName, bool isStoredProcedure)
         {
             string connectionString = _config.GetConnectionString(connectionStringName);
             CommandType commandType = CommandType.Text;
@@ -42,13 +48,10 @@ namespace BlogDataLibrary.Database
             {
                 commandType = CommandType.StoredProcedure;
             }
-            using (IDbConnection connection = new SqlConnection(connectionString))
+            using (IDbConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = BlogDB; Integrated Security=True;Connect Timeout=60;"))
             {
                 connection.Execute(sqlStatement, parameters, commandType: commandType);
             }
         }
     }
-}
-
-
 }
--- SANTOSLE2/BlogDataLibrary/Models/PostForm.cs
+++ SANTOSLE2/BlogDataLibrary/Models/PostForm.cs
@@ -6,12 +6,9 @@ using System.Threading.Tasks;
 
 namespace BlogDataLibrary.Models
 {
-    public class PostModel
+    public class PostForm
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
-        public DateTime DateCreated { get; set; }
     }
 }
